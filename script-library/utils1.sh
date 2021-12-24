833a80b78ba4dd25f014525212df2d5d8ce3bbb2
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2021-12-24 14:46:25 +0100

    style: Remove comments

--- script-library/utils1.sh
+++ script-library/utils1.sh
@@ -14,21 +14,10 @@ if [[ -d "/var/lib/apt/lists" && $(ls /var/lib/apt/lists/ | wc -l) -eq 0 ]]; the
   fi
 fi
 
-# if [[ $(apt list --installed locales 2> /dev/null | wc -l) -lt 2 ]]; then
-#   if sudo apt-get install --no-install-recommends --assume-yes locales > /dev/null 2>&1; then
-#     echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen && sudo locale-gen
-#   fi
-# fi
-
 if df --type=btrfs / > /dev/null 2>&1 && ! command -v btrfs > /dev/null 2>&1; then
   sudo apt-get install --no-install-recommends --assume-yes \
     btrfs-progs
 fi
-
-# USERNAME="vscode"
-# if getent passwd | grep "$USERNAME" > /dev/null 2>&1 && [[ ! -e /etc/sudoers.d/$USERNAME ]]; then
-#   echo $USERNAME ALL=\(root\) NOPASSWD:ALL > /etc/sudoers.d/$USERNAME && chmod 0440 /etc/sudoers.d/$USERNAME
-# fi
 
 if [[ $(ls /var/lib/apt/lists/ | wc -l) -ne 0 ]]; then
   sudo apt-get clean
